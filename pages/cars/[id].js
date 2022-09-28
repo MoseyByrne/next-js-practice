278042220a91c32a7d5273ffd0533b698aefd786
MoseyByrne <mosey.b.byrne@gmail.com>
2022-09-28 12:10:45 -0700

    add pages/cars

---
+++ pages/cars/[id].js
@@ -0,0 +1,7 @@
+// then for each individual car
+// we'll add a component that has a file
+// name of [paramname].js
+// the brackets make this route dynamic
+// which means anytime a user navigates to cars
+// slash whatever (cars slash tesla)
+// it will render the component in this file.
